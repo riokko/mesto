f584a047f951bb56afed972a869a01e2d51e08f5
Daria Barova <breya@yandex.ru>
2020-09-09 09:48:39 +0200

    fix webpacks errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: { main: './src/scripts/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    },
+  entry: { main: "./src/scripts/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js"
+  },
     module: {
         rules: [
             {
@@ -17,21 +17,30 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader:  [MiniCssExtractPlugin.loader, 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|gif|woff2)$/,
-                loader: 'file-loader'
+                loader: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    'postcss-loader'
+                ],
             },
             {
                 test: /\.html$/,
                 loader: 'html-loader',
             },
+            {
+                test: /\.(png|svg|jpg|gif|woff2|woff)$/,
+                loader: 'file-loader',
+            },
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: 'src/index.html'
         }),
         new MiniCssExtractPlugin()
     ]
